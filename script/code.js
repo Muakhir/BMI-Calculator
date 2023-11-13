c5f880ad3705bf14435ae3da4af977a1b4fb039c
Muakhir <almuakhirjones@gmail.com>
2023-11-13 14:58:40 +0200

    1.0

---
+++ script/code.js
@@ -0,0 +1,11 @@
+function calculateBMI() {
+    let weight = document.getElementById('weight').value;
+    let height = document.getElementById('height').value;
+
+    if (weight && height) {
+        let bmi = (weight / ((height / 100) * (height / 100))).toFixed(2);
+        document.getElementById('result').innerText = 'Your BMI is: ' + bmi;
+    } else {
+        alert('Please enter both weight and height.');
+    }
+}
