1a986e852d052bc2ebbe54ae593ff0e0b250f34e
unknown <almuakhirjones@gmail.com>
2023-11-13 19:51:07 +0200

    fix1.2

--- script/code.js
+++ script/code.js
@@ -3,10 +3,11 @@ let weight= document.getElementById('weight').value;
 let height= document.getElementById('height').value;
 
 
- if (weight,height) {
+ if (weight && height) {
      let bmi = (weight / ((height / 100) * (height / 100))).toFixed(2)
      document.getElementById('result').innerText = 'Your BMI is: ' + bmi;
 
 } else {
-    alert('Please enter both weight and height.');
+    alert('Please enter both weight and height.')
+    }
 }
