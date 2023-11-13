1fbd15168fe699efa5ee0bd6aad03005dec2e103
unknown <almuakhirjones@gmail.com>
2023-11-13 19:40:59 +0200

    fix

--- script/code.js
+++ script/code.js
@@ -1,11 +1,12 @@
 function calculateBMI() {
-    let weight = document.getElementById('weight').value;
-    let height = document.getElementById('height').value;
+let weight= document.getElementById('weight').value;
+let height= document.getElementById('height').value;
 
-    if (weight && height) {
-        let bmi = (weight / ((height / 100) * (height / 100))).toFixed(2);
-        document.getElementById('result').innerText = 'Your BMI is: ' + bmi;
-    } else {
-        alert('Please enter both weight and height.');
-    }
+
+ if (weight,height) {
+     let bmi = (weight / ((height / 100) * (height / 100))).toFixed(2)
+     document.getElementById('result').innerText = 'Your BMI is: ' + bmi;
+
+} else {
+    alert('Please enter both weight and height.');
 }
